9b68198fddea7a1576212ab76850db91b7b37180
ayushisharma-05 <ayushi.sharma5205@gmail.com>
2026-04-13 22:10:23 +0530

    Updated Files

--- Stop Watch/script.js
+++ Stop Watch/script.js
@@ -1,4 +1,3 @@
-
 var start = document.getElementById("start");
 var stop = document.getElementById("stop");
 var reset = document.getElementById("reset");
@@ -6,53 +5,70 @@ var min = document.getElementById("min");
 var sec = document.getElementById("sec");
 var milli = document.getElementById("milli");
 
-var millicount = 0, seccount = 0, mincount = 0, watch;
+var millicount = 0, seccount = 0, mincount = 0, watch = false;
+
+let timer = null;
+
 start.addEventListener("click", () => {
+    if (timer !== null) return; 
+
     watch = true;
-    stopwatch();
-    document.getElementById("start").innerHTML = "Start";
-    document.getElementById("stop").innerHTML = "Stop";
+    timer = setInterval(stopwatch, 10);
+
+    start.innerHTML = "Start";
+    stop.innerHTML = "Stop";
 });
 
 reset.addEventListener("click", () => {
+    clearInterval(timer);
+    timer = null;
+
     watch = false;
-    min.innerHTML = "00" + "&nbsp:&nbsp";
+
+    min.innerHTML = "00&nbsp;:&nbsp;";
+    sec.innerHTML = "00&nbsp;:&nbsp;";
     milli.innerHTML = "00";
-    sec.innerHTML = "00" + "&nbsp:&nbsp";
+
     mincount = 0;
     seccount = 0;
     millicount = 0;
-    document.getElementById("start").innerHTML = "Start";
-    document.getElementById("stop").innerHTML = "Stop";
 
-})
+    start.innerHTML = "Start";
+    stop.innerHTML = "Stop";
+});
 
 stop.addEventListener("click", () => {
-    if (millicount != 0 || seccount != 0 || mincount != 0) {
-        document.getElementById("start").innerHTML = "Resume";
-        document.getElementById("stop").innerHTML = "Pause";
-        watch = false;// stopwatch function har 1 sec bad chal hi rha h bas apanne stop hit kiya to eventlistner ne wacth me false set kdia and stopwatch()ki condition break hui and vo band hoya
+    if (timer !== null) {
+        clearInterval(timer);
+        timer = null;
 
+        watch = false;
 
+        start.innerHTML = "Resume";
+        stop.innerHTML = "Pause";
     }
-})
+});
+
 function stopwatch() {
     if (watch) {
         millicount++;
-        if (millicount == 99) {
+
+        if (millicount === 100) {
             seccount++;
             millicount = 0;
         }
-        if (seccount == 59) {
+
+        if (seccount === 60) {
             mincount++;
             seccount = 0;
         }
+
         milli.innerHTML = (millicount < 10 ? "0" + millicount : millicount);
-        sec.innerHTML = (seccount < 10 ? "0" + seccount + "&nbsp:&nbsp" : seccount + "&nbsp:&nbsp");
-        min.innerHTML = (mincount < 10 ? "0" + mincount + "&nbsp:&nbsp" : mincount + "&nbsp:&nbsp");
+        sec.innerHTML = (seccount < 10 ? "0" + seccount : seccount) + "&nbsp;:&nbsp;";
+        min.innerHTML = (mincount < 10 ? "0" + mincount : mincount) + "&nbsp;:&nbsp;";
     }
-    setTimeout(stopwatch, 10)
 }
+
 
 
 // start.addEventListener("click",()=>{
